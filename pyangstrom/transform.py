5c354f70a04d8d7ef6d6ea9decf21274846ca95f
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-03-28 22:22:17 -0700

    fix axes labels and account for time axis

--- pyangstrom/transform.py
+++ pyangstrom/transform.py
@@ -11,7 +11,7 @@ TEMPERATURE_OFFSET = {
 class Region:
     def __init__(self) -> None:
         """Always in absolute temperature (expect Kelvin)"""
-        self.margins: NamedTuple = None
+        self.margins: NamedTuple = None # Should be in (dist, ..., time) order
 
 class Geometry:
     pass
@@ -37,8 +37,8 @@ def extract_polar_region(
         df_recording: pd.DataFrame,
         geometry: PolarGeometry,
 ) -> Region:
-    temps = np.stack(df_recording['Samples']) # (..., height, width)
-    temps = np.moveaxis(temps, [-1, -2], [0, 1]) # (width, height, ...)
+    temps = np.stack(df_recording['Samples']) # (time, ..., height, width)
+    temps = np.moveaxis(temps, [0, -1, -2], [-1, 0, 1]) # (width, height, ..., time)
 
     r = np.linspace(
         geometry.min_r,
@@ -65,23 +65,24 @@ def extract_polar_region(
     lower_y_weight = (y_coord_floorp1 - y_coord) / (y_coord_floorp1 - y_coord_floor)
     upper_y_weight = (y_coord - y_coord_floor) / (y_coord_floorp1 - y_coord_floor)
 
-    # (..., width, height)
+    # (..., time, radius, angle)
     lxly_temps = np.moveaxis(temps[x_coord_floor, y_coord_floor], [0, 1], [-2, -1]) * lower_x_weight * lower_y_weight
     lxuy_temps = np.moveaxis(temps[x_coord_floor, y_coord_floorp1], [0, 1], [-2, -1]) * lower_x_weight * upper_y_weight
     uxly_temps = np.moveaxis(temps[x_coord_floorp1, y_coord_floor], [0, 1], [-2, -1]) * upper_x_weight * lower_y_weight
     uxuy_temps = np.moveaxis(temps[x_coord_floorp1, y_coord_floorp1], [0, 1], [-2, -1]) * upper_x_weight * upper_y_weight
 
     temps_trans = lxly_temps + lxuy_temps + uxly_temps + uxuy_temps
-
+    temps_trans = np.moveaxis(temps_trans, [-2, -1], [0, 1]) # (radius, angle, ..., time)
     temps_trans = add_temperature_offset(df_recording, temps_trans)
 
     region = Region(
         df_recording.index,
         temps_trans,
         (
-            ...,
             geometry.max_r - geometry.min_r,
             geometry.max_theta - geometry.min_theta,
+            ...,
+            df_recording.index.max() - df_recording.index.min(),
         ),
     )
     return region
