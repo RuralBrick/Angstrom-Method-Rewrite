89679263817988caa5c00b8e244c42fb5f81731a
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2024-04-02 14:42:25 -0700

    properly extract regions

--- pyangstrom/transform.py
+++ pyangstrom/transform.py
@@ -1,4 +1,6 @@
 from typing import NamedTuple
+from enum import Enum, auto
+from dataclasses import dataclass
 
 import pandas as pd
 import numpy as np
@@ -8,18 +10,43 @@ TEMPERATURE_OFFSET = {
     'Temperature (C)': 273.15,
 }
 
+class Direction(Enum):
+    LESSER_X = auto()
+    GREATER_X = auto()
+    LESSER_Y = auto()
+    GREATER_Y = auto()
+
+@dataclass
+class Point:
+    x: int | float
+    y: int | float
+
+@dataclass
+class CartesianGeometry:
+    min_x: int
+    max_x: int
+    min_y: int
+    max_y: int
+    heat_source: Direction
+
+@dataclass
+class PolarGeometry:
+    center: Point
+    min_r: int | float
+    max_r: int | float
+    num_r: int
+    min_theta: int | float
+    max_theta: int | float
+    num_theta: int
+
+Geometry = CartesianGeometry | PolarGeometry
+
+@dataclass
 class Region:
-    def __init__(self) -> None:
-        """Always in absolute temperature (expect Kelvin)"""
-        self.time: pd.DatetimeIndex = None
-        self.temps: np.ndarray = None
-        self.margins: NamedTuple = None # Should be in (dist, ..., time) order
-
-class Geometry:
-    pass
-
-class PolarGeometry(Geometry):
-    pass
+    """Always in absolute temperature (expect Kelvin)"""
+    time: pd.DatetimeIndex
+    temps: np.ndarray
+    margins: NamedTuple # Should be in (dist, ..., time) order
 
 
 def add_temperature_offset(
@@ -34,6 +61,50 @@ def add_temperature_offset(
         msg = (f"Temperature unit \"{df_recording['Units'].unique().item()}\" "
                "not accounted for")
         raise NotImplementedError(msg)
+
+def extract_cartesian_region(
+        df_recording: pd.DataFrame,
+        geometry: CartesianGeometry,
+) -> Region:
+    temps = np.stack(df_recording['Samples']) # (time, ..., height, width)
+    temps = np.moveaxis(temps, [0, -1, -2], [-1, 0, 1]) # (width, height, ..., time)
+    temps = temps[
+        geometry.min_x : geometry.max_x + 1,
+        geometry.min_y : geometry.max_y + 1,
+    ]
+
+    # (displacement, span, ..., time)
+    match geometry.heat_source:
+        case Direction.LESSER_X:
+            displacement_coverage = geometry.max_x - geometry.min_x
+            span_coverage = geometry.max_y - geometry.min_y
+        case Direction.GREATER_X:
+            temps = np.flip(temps, axis=0)
+            displacement_coverage = geometry.max_x - geometry.min_x
+            span_coverage = geometry.max_y - geometry.min_y
+        case Direction.LESSER_Y:
+            temps = np.swapaxes(temps, 0, 1)
+            displacement_coverage = geometry.max_y - geometry.min_y
+            span_coverage = geometry.max_x - geometry.min_x
+        case Direction.GREATER_Y:
+            temps = np.swapaxes(temps, 0, 1)
+            temps = np.flip(temps, axis=0)
+            displacement_coverage = geometry.max_y - geometry.min_y
+            span_coverage = geometry.max_x - geometry.min_x
+
+    temps = add_temperature_offset(df_recording, temps)
+
+    region = Region(
+        df_recording.index,
+        temps,
+        (
+            displacement_coverage,
+            span_coverage,
+            ...,
+            df_recording.index.max() - df_recording.index.min(),
+        ),
+    )
+    return region
 
 def extract_polar_region(
         df_recording: pd.DataFrame,
@@ -52,7 +123,7 @@ def extract_polar_region(
         geometry.max_theta,
         geometry.num_theta,
     ))
-    r_coord, theta_coord = np.meshgrid(r, theta)
+    r_coord, theta_coord = np.meshgrid(r, theta, indexing='ij')
 
     x_coord = r_coord * np.cos(theta_coord) + geometry.center.x
     y_coord = r_coord * np.sin(theta_coord) + geometry.center.y
--- pyangstrom/visualization.py
+++ pyangstrom/visualization.py
@@ -1,11 +1,103 @@
 import pandas as pd
+import numpy as np
+from matplotlib.axes import Axes
+from matplotlib.patches import Rectangle, Wedge, Circle
 import matplotlib.pyplot as plt
 from matplotlib.animation import Animation, FuncAnimation
 
+from pyangstrom.transform import Direction, CartesianGeometry, PolarGeometry
+
+
+def plot_recording(ax: Axes, df_recording: pd.DataFrame) -> Axes:
+    ax.imshow(df_recording['Samples'].sum())
+    return ax
+
+def plot_cartesian_geometry(
+        ax: Axes,
+        df_recording: pd.DataFrame,
+        geometry: CartesianGeometry,
+) -> Axes:
+    ax = plot_recording(ax, df_recording)
+    ax.add_patch(Rectangle(
+        (geometry.min_x, geometry.min_y),
+        geometry.max_x - geometry.min_x,
+        geometry.max_y - geometry.min_y,
+        hatch='..',
+        edgecolor='red',
+        facecolor='none',
+    ))
+    match geometry.heat_source:
+        case Direction.LESSER_X:
+            ax.plot(
+                [geometry.min_x, geometry.min_x],
+                [geometry.min_y, geometry.max_y],
+                linewidth=2,
+                color='blue',
+            )
+        case Direction.GREATER_X:
+            ax.plot(
+                [geometry.max_x, geometry.max_x],
+                [geometry.min_y, geometry.max_y],
+                linewidth=2,
+                color='blue',
+            )
+        case Direction.LESSER_Y:
+            ax.plot(
+                [geometry.min_x, geometry.max_x],
+                [geometry.min_y, geometry.min_y],
+                linewidth=2,
+                color='blue',
+            )
+        case Direction.GREATER_Y:
+            ax.plot(
+                [geometry.min_x, geometry.max_x],
+                [geometry.max_y, geometry.max_y],
+                linewidth=2,
+                color='blue',
+            )
+    return ax
+
+def plot_polar_geometry(
+        ax: Axes,
+        df_recording: pd.DataFrame,
+        geometry: PolarGeometry,
+) -> Axes:
+    ax = plot_recording(ax, df_recording)
+    ax.add_patch(Wedge(
+        (geometry.center.x, geometry.center.y),
+        geometry.max_r,
+        geometry.min_theta,
+        geometry.max_theta,
+        hatch='..',
+        edgecolor='red',
+        facecolor='none',
+    ))
+    ax.add_patch(Circle(
+        (geometry.center.x, geometry.center.y),
+        geometry.min_r,
+        linewidth=2,
+        edgecolor='r',
+        linestyle='solid',
+        facecolor='none',
+    ))
+    ax.add_patch(Circle(
+        (geometry.center.x, geometry.center.y),
+        geometry.max_r,
+        linewidth=2,
+        edgecolor='r',
+        linestyle='solid',
+        facecolor='none',
+    ))
+    return ax
 
 def animate_recording(df_recording: pd.DataFrame) -> Animation:
     fig, ax = plt.subplots()
-    img = ax.imshow(df_recording['Samples'].iloc[0])
+    arr_temp = np.stack(df_recording['Samples'])
+    img = ax.imshow(
+        df_recording['Samples'].iloc[0],
+        vmin=arr_temp.min(),
+        vmax=arr_temp.max(),
+    )
     def update(samples):
         time, frame = samples
         img.set_data(frame)
