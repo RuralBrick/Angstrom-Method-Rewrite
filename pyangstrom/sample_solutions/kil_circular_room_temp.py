8c61a1793a9dc74fd986ac351b2bc7242d1452dd
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-05-24 10:28:52 -0700

    some calculation

--- pyangstrom/sample_solutions/kil_circular_room_temp.py
+++ pyangstrom/sample_solutions/kil_circular_room_temp.py
@@ -1,17 +1,35 @@
 from typing import NamedTuple
 
 import numpy as np
+from scipy.special import jv, yv
 
 from pyangstrom.fit import ExperimentalSetup, SignalProperties
 
 
 class KilCircularRoomTempUnknowns(NamedTuple):
     thermal_diffusivity_m2_s: float
-    thermal_transfer_coefficient_kg_s2_K_m2: float
+    convective_heat_transfer_term: float
+
+def calc_convective_heat_transfer_coefficient():
+    pass
 
 def solve(
         unknowns: KilCircularRoomTempUnknowns,
         displacements_meters: np.ndarray,
         setup: ExperimentalSetup,
+        min_radius_meters: float,
+        max_radius_meters: float,
 ) -> SignalProperties:
+    thermal_diffusivity_m2_s, convective_heat_transfer_term = unknowns
+    angular_frequency = 2.0 * np.pi * setup['heating_frequency_hertz']
+
+    temp_var1 = convective_heat_transfer_term * convective_heat_transfer_term
+    temp_var2 = 1j * angular_frequency / thermal_diffusivity_m2_s
+
+    temp_var3 = np.sqrt(temp_var1 + temp_var2)
+    temp_var4 = None
+
+    temp_var5 = np.sqrt(temp_var1 - temp_var2)
+
+
     raise NotImplementedError()
