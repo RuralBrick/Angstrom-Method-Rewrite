0cd82cb5ca508311d4056fecaad9c0b5b72e716e
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-05-28 19:12:57 -0700

    Use new api

--- pyangstrom/sample_solutions/kil_circular_room_temp.py
+++ pyangstrom/sample_solutions/kil_circular_room_temp.py
@@ -1,17 +1,18 @@
-from typing import NamedTuple
+from typing import TypedDict
 
 import numpy as np
 from scipy.special import jv, yv
 
-from pyangstrom.fit import ExperimentalSetup, SignalProperties
+from pyangstrom.exp_setup import ExperimentalSetup
+from pyangstrom.signal import RegionProperties, SignalProperties
+from pyangstrom.fitting_methods.nelder_mead import NelderMeadEquations
+from pyangstrom.fitting_methods.lsr import LsrEquations
+from pyangstrom.fitting_methods.metropolis_hastings import MetropolisHastingsEquations
 
 
-class KilCircularRoomTempUnknowns(NamedTuple):
+class KilCircularRoomTempUnknowns(TypedDict):
     thermal_diffusivity_m2_s: float
     convective_heat_transfer_term: float
-
-def calc_convective_heat_transfer_coefficient():
-    pass
 
 def J0(x):
     return jv(0, x)
@@ -19,94 +20,83 @@ def J0(x):
 def Y0(x):
     return yv(0, x)
 
-def calc_normalized_T1(
-        radii_meters,
-        min_radius_meters,
-        max_radius_meters,
-        angular_frequency_hertz,
-        thermal_diffusivity_m2_s,
-        convective_heat_transfer_term,
-):
-    i = 1j
-    r = radii_meters
-    a = min_radius_meters
-    b = max_radius_meters
-    w = angular_frequency_hertz
-    A = thermal_diffusivity_m2_s
-    m = convective_heat_transfer_term
+class Solution(NelderMeadEquations, LsrEquations, MetropolisHastingsEquations):
+    def __init__(
+            self,
+            region_properties: RegionProperties,
+            setup: ExperimentalSetup,
+    ) -> None:
+        # HACK: Actually need to use 2D array
+        self.time_seconds = region_properties.seconds_elapsed
+        self.radii_meters = region_properties.displacements_meters
+        # end HACK
+        self.angular_frequency_hertz = 2*np.pi*setup['heating_frequency_hertz']
+        self.min_radius_meters = region_properties.displacements_meters[0]
+        self.max_radius_meters = region_properties.displacements_meters[-1]
 
-    X = i * np.sqrt(m*m + i*w/A)
-    numerator = -Y0(-b*X) * J0(r*X) + J0(b*X) * Y0(-r*X)
-    denominator = J0(b*X) * Y0(-a*X) - J0(a*X) * Y0(-b*X)
-    T1 = 0.5 * numerator / denominator
+    def calc_convective_heat_transfer_coefficient(self):
+        raise NotImplementedError()
 
-    return T1
+    def calc_normalized_T1(
+            self,
+            thermal_diffusivity_m2_s,
+            convective_heat_transfer_term,
+    ):
+        i = 1j
+        r = self.radii_meters
+        a = self.min_radius_meters
+        b = self.max_radius_meters
+        w = self.angular_frequency_hertz
+        D = thermal_diffusivity_m2_s
+        m = convective_heat_transfer_term
 
-def calc_normalized_T2(
-        radii_meters,
-        min_radius_meters,
-        max_radius_meters,
-        angular_frequency_hertz,
-        thermal_diffusivity_m2_s,
-        convective_heat_transfer_term,
-):
-    i = 1j
-    r = radii_meters
-    a = min_radius_meters
-    b = max_radius_meters
-    w = angular_frequency_hertz
-    A = thermal_diffusivity_m2_s
-    m = convective_heat_transfer_term
+        X = i * np.sqrt(m*m + i*w/D)
+        numerator = -Y0(-b*X) * J0(r*X) + J0(b*X) * Y0(-r*X)
+        denominator = J0(b*X) * Y0(-a*X) - J0(a*X) * Y0(-b*X)
+        T1 = 0.5 * numerator / denominator
 
-    X = i * np.sqrt(m*m - i*w/A)
-    numerator = -Y0(-b*X) * J0(r*X) + J0(b*X) * Y0(-r*X)
-    denominator = J0(b*X) * Y0(-a*X) - J0(a*X) * Y0(-b*X)
-    T2 = 0.5 * numerator / denominator
+        return T1
 
-    return T2
+    def calc_normalized_T2(
+            self,
+            thermal_diffusivity_m2_s,
+            convective_heat_transfer_term,
+    ):
+        i = 1j
+        r = self.radii_meters
+        a = self.min_radius_meters
+        b = self.max_radius_meters
+        w = self.angular_frequency_hertz
+        A = thermal_diffusivity_m2_s
+        m = convective_heat_transfer_term
 
-def calc_normalized_temps(
-        radii_meters,
-        time_seconds,
-        min_radius_meters,
-        max_radius_meters,
-        angular_frequency_hertz,
-        thermal_diffusivity_m2_s,
-        convective_heat_transfer_term,
-):
-    i = 1j
-    t = time_seconds
-    w = angular_frequency_hertz
-    T1 = calc_normalized_T1(
-        radii_meters,
-        min_radius_meters,
-        max_radius_meters,
-        angular_frequency_hertz,
-        thermal_diffusivity_m2_s,
-        convective_heat_transfer_term,
-    )
-    T2 = calc_normalized_T2(
-        radii_meters,
-        min_radius_meters,
-        max_radius_meters,
-        angular_frequency_hertz,
-        thermal_diffusivity_m2_s,
-        convective_heat_transfer_term,
-    )
+        X = i * np.sqrt(m*m - i*w/A)
+        numerator = -Y0(-b*X) * J0(r*X) + J0(b*X) * Y0(-r*X)
+        denominator = J0(b*X) * Y0(-a*X) - J0(a*X) * Y0(-b*X)
+        T2 = 0.5 * numerator / denominator
 
-    T = T1 * np.exp(i*w*t) + T2 * np.exp(-i*w*t)
+        return T2
 
-    return T
+    def calc_normalized_temps(
+            self,
+            thermal_diffusivity_m2_s,
+            convective_heat_transfer_term,
+    ):
+        i = 1j
+        t = self.time_seconds
+        w = self.angular_frequency_hertz
+        T1 = self.calc_normalized_T1(
+            thermal_diffusivity_m2_s,
+            convective_heat_transfer_term,
+        )
+        T2 = self.calc_normalized_T2(
+            thermal_diffusivity_m2_s,
+            convective_heat_transfer_term,
+        )
 
-def solve(
-        unknowns: KilCircularRoomTempUnknowns,
-        displacements_meters: np.ndarray,
-        setup: ExperimentalSetup,
-        min_radius_meters: float,
-        max_radius_meters: float,
-) -> SignalProperties:
-    thermal_diffusivity_m2_s, convective_heat_transfer_term = unknowns
-    angular_frequency = 2.0 * np.pi * setup['heating_frequency_hertz']
+        T = T1 * np.exp(i*w*t) + T2 * np.exp(-i*w*t)
 
+        return T
 
-    raise NotImplementedError()
+    def solve(self, unknowns: KilCircularRoomTempUnknowns) -> SignalProperties:
+        raise NotImplementedError()
