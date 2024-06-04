d4dcff0b465f57c9a2f19101c9aaa53abe7a3694
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-06-03 21:54:11 -0700

    Option to choose which properties to use in nelder-mead

--- pyangstrom/fitting_methods/nelder_mead.py
+++ pyangstrom/fitting_methods/nelder_mead.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 import abc
 
 import numpy as np
@@ -15,6 +16,11 @@ from pyangstrom.signal import SignalProperties
 
 logger = logging.getLogger('fit')
 
+class UsedProperties(Enum):
+    AMPLITUDE_RATIOS_AND_PHASE_DIFFERENCES = 'phase-amplitude'
+    AMPLITUDE_RATIOS_ONLY = 'amplitude'
+    PHASE_DIFFERENCES_ONLY = 'phase'
+
 class NelderMeadEquations(EquationPackage):
     @abc.abstractmethod
     def unknowns_to_vector(self, unknowns: Unknowns) -> np.ndarray: ...
@@ -25,20 +31,38 @@ class NelderMeadEquations(EquationPackage):
     @abc.abstractmethod
     def vector_solve(self, unknowns_vector: np.ndarray) -> SignalProperties: ...
 
+def extract_used_properties(
+        properties: SignalProperties,
+        properties_to_use: str | UsedProperties,
+) -> np.ndarray:
+    properties_to_use = UsedProperties(properties_to_use)
+    match properties_to_use:
+        case UsedProperties.AMPLITUDE_RATIOS_AND_PHASE_DIFFERENCES:
+            return np.stack(properties)
+        case UsedProperties.AMPLITUDE_RATIOS_ONLY:
+            return properties.amplitude_ratios
+        case UsedProperties.PHASE_DIFFERENCES_ONLY:
+            return properties.phase_differences
+
 def fit(
         unknowns_guesses: Unknowns,
         solution: NelderMeadEquations,
         observed_properties: SignalProperties,
+        properties_to_use: str | UsedProperties = UsedProperties.AMPLITUDE_RATIOS_AND_PHASE_DIFFERENCES,
         **minimize_kwargs,
 ) -> FittingResult:
+    used_observed_properties = extract_used_properties(
+        observed_properties,
+        properties_to_use,
+    )
 
     def calc_error(unknowns_vector):
         theoretical_properties = solution.vector_solve(unknowns_vector)
-        all_residuals = [
-            o - t for o, t
-            in zip(observed_properties, theoretical_properties)
-        ]
-        residuals = np.stack(all_residuals).flatten()
+        used_theoretical_properties = extract_used_properties(
+            theoretical_properties,
+            properties_to_use,
+        )
+        residuals = used_observed_properties - used_theoretical_properties
         error = np.sum(np.square(residuals))
         return error
 
