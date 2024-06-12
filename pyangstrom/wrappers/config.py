ae789aad04f7807fac23a1515abc594a77b604d8
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2024-06-12 16:27:25 -0700

    update kil unknowns units

--- pyangstrom/wrappers/config.py
+++ pyangstrom/wrappers/config.py
@@ -125,7 +125,7 @@ def record_to_kil_circular_room_temp(record) -> Optional[SolverInformation]:
         solver: SolverInformation = {
             'name': 'kil',
             'guesses': {
-                'thermal_diffusivity_m2_s': 1,
+                'thermal_diffusivity_m2__s': 1,
                 'convective_heat_transfer_term': 35,
             },
         }
